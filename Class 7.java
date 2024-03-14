c791250d42b390c9266ab68a1787e125e6d6fbd4
Pragya Vijay <118822753+PragyaVijay1222@users.noreply.github.com>
2024-03-14 20:25:03 +0530

    Create Class 7.java

---
+++ Class 7.java
@@ -0,0 +1,8 @@
+public class heap_and_stack {
+    public static void main(String[] args){
+        //We want to create an object out of variable x
+        int x = 5;
+        Integer x1=new Integer(x);
+        System.out.println(x1);
+    }
+}
